6a01db54327bcedbdd0f7afd1af6d1a6897a311d
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-11-13 01:36:48 -0500

    created project

---
+++ CloudKitStruct/CloudKitStruct/CloudKitStruct.h
@@ -0,0 +1,17 @@
+//
+//  CloudKitStruct.h
+//  CloudKitStruct
+//
+//  Created by Alsey Coleman Miller on 11/13/15.
+//  Copyright © 2015 ColemanCDA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CloudKitStruct.
+FOUNDATION_EXPORT double CloudKitStructVersionNumber;
+
+//! Project version string for CloudKitStruct.
+FOUNDATION_EXPORT const unsigned char CloudKitStructVersionString[];
+
+
---
+++ CloudKitStruct/CloudKitStruct/Encode.swift
@@ -0,0 +1,15 @@
+//
+//  Encode.swift
+//  CloudKitStruct
+//
+//  Created by Alsey Coleman Miller on 11/13/15.
+//  Copyright © 2015 ColemanCDA. All rights reserved.
+//
+
+import CloudKit
+
+/// Specifies how a type can be encoded to be stored with CloudKit.
+public protocol CloudKitEncodable {
+    
+    func save(record: CKDatabase)
+}
